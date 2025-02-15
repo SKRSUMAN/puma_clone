efe7f397c0fa86032ba08195ac2eb67df91d3c37
SKRSUMAN <sumankhairba2002@gmail.com>
2025-02-15 18:41:31 +0530

    Upated Code

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import AccordianFooter from "@/components/Footer/FooterAccordian/AccordianFooter";
 import CountrySearch from "@/components/Footer/India/CountrySearch";
 import { AboutDatas } from "@/constant/Footer/About";
 import { PaymentIconsDatas } from "@/constant/Footer/PaymentIcons";
@@ -18,81 +19,94 @@ const Footer = () => {
     setCountry(!country);
   };
 
-  return (
-    <div className="relative w-full bg-[#181818] text-white">
-      <div className="grid grid-cols-4 gap-5 px-30 pt-10 pb-2">
-        <div className="grid grid-cols-2 col-span-2 items-start">
-          <div className="flex flex-col gap-2">
-            <h1 className="tracking-[1px] text-[18px] font-bold">
-              {"Support".toUpperCase()}
-            </h1>
-            {SupportDatas.slice(0, 8).map((data, index) => (
-              <Link href={data.href} key={index}>
-                <p className="text-[rgb(215,211,211)] hover:text-white tracking-[1px] text-[15px] place-self-start">
-                  {data.name}
-                </p>
-              </Link>
-            ))}
-          </div>
-          <div className="flex flex-col gap-2 my-10">
-            {SupportDatas.slice(8).map((data, index) => (
-              <Link href={data.href} key={index + 8}>
-                <p className="text-[rgb(215,211,211)] hover:text-white tracking-[1px] text-[15px] place-self-start">
-                  {data.name}
-                </p>
-              </Link>
-            ))}
-          </div>
-        </div>
+  const AccordianTitle = ["SUPPORT", "ABOUT", "STAY UP TO DATE", "EXPLORE"];
 
-        <div>
-          <h1 className="tracking-[1px] text-[18px] font-bold">ABOUT</h1>
-          <div className="flex flex-col gap-2 my-2">
-            {AboutDatas.map((data, index) => (
-              <Link href={data.href} key={index}>
-                <p className="text-[rgb(215,211,211)] hover:text-white tracking-[1px] text-[15px]">
-                  {data.name}
-                </p>
-              </Link>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col gap-10 ">
-          <div className="flex flex-col gap-7">
-            <h1 className="text-[18px] font-bold">
-              {"Stay up to date".toUpperCase()}
-            </h1>
-            <div className="flex gap-5 items-center">
-              {SocialMediaDatas.map((data, index) => (
-                <Link
-                  href={data.href}
-                  key={index}
-                  className="w-11 h-11 flex flex-col items-center justify-center rounded-full cursor-pointer hover:border-white hover:bg-[#404040]">
-                  <p className="p-5">{data.img}</p>
+  return (
+    <div className="w-full bg-[#181818] text-white  ">
+      <div className="relative xl:px-25 lg:px-15 px-10 hidden lg:block">
+        <div className="grid grid-cols-4 gap-5 pt-10 pb-2">
+          <div className="grid grid-cols-2 col-span-2 items-start">
+            <div className="flex flex-col gap-2">
+              <h1 className="tracking-[1px] text-[18px] font-bold">
+                {"Support".toUpperCase()}
+              </h1>
+              {SupportDatas.slice(0, 8).map((data, index) => (
+                <Link href={data.href} key={index}>
+                  <p className="text-[rgb(215,211,211)] hover:text-white tracking-[1px] text-[15px] place-self-start">
+                    {data.name}
+                  </p>
+                </Link>
+              ))}
+            </div>
+            <div className="flex flex-col gap-2 my-10">
+              {SupportDatas.slice(8).map((data, index) => (
+                <Link href={data.href} key={index + 8}>
+                  <p className="text-[rgb(215,211,211)] hover:text-white tracking-[1px] text-[15px] place-self-start">
+                    {data.name}
+                  </p>
                 </Link>
               ))}
             </div>
           </div>
-          <div className="flex flex-col gap-5">
-            <h1 className="text-[18px] font-bold">{"Explore".toUpperCase()}</h1>
-            <div className="flex gap-10">
-              <div className="border border-[rgb(102,98,98)] w-[65px] h-[65px] rounded-md flex flex-col items-center justify-center p-2 cursor-pointer hover:border-white">
-                <SiPuma className="w-15 h-15" />
-                <p className="tracking-[2px] text-[12px]">App</p>
+
+          <div className="">
+            <h1 className="tracking-[1px] text-[18px] font-bold">ABOUT</h1>
+            <div className="flex flex-col gap-2 my-2">
+              {AboutDatas.map((data, index) => (
+                <Link href={data.href} key={index}>
+                  <p className="text-[rgb(215,211,211)] hover:text-white tracking-[1px] text-[15px]">
+                    {data.name}
+                  </p>
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col gap-10 ">
+            <div className="flex flex-col gap-7">
+              <h1 className="text-[18px] font-bold">
+                {"Stay up to date".toUpperCase()}
+              </h1>
+              <div className="flex gap-5 items-center">
+                {SocialMediaDatas.map((data, index) => (
+                  <Link
+                    href={data.href}
+                    key={index}
+                    className="w-11 h-11 flex flex-col items-center justify-center rounded-full cursor-pointer hover:border-white hover:bg-[#404040]">
+                    <p className="p-5">{data.img}</p>
+                  </Link>
+                ))}
               </div>
-              <div className="border border-[rgb(102,98,98)] w-[65px] h-[65px] rounded-md flex flex-col items-center justify-center p-2 cursor-pointer hover:border-white">
-                <SiPuma className="w-15 h-15" />
-                <p className="tracking-[2px] text-[12px]">TRAC</p>
+            </div>
+            <div className="flex flex-col gap-5">
+              <h1 className="text-[18px] font-bold">
+                {"Explore".toUpperCase()}
+              </h1>
+              <div className="flex gap-10">
+                <div className="border border-[rgb(102,98,98)] w-[65px] h-[65px] rounded-md flex flex-col items-center justify-center p-2 cursor-pointer hover:border-white">
+                  <SiPuma className="w-15 h-15" />
+                  <p className="tracking-[2px] text-[12px]">App</p>
+                </div>
+                <div className="border border-[rgb(102,98,98)] w-[65px] h-[65px] rounded-md flex flex-col items-center justify-center p-2 cursor-pointer hover:border-white">
+                  <SiPuma className="w-15 h-15" />
+                  <p className="tracking-[2px] text-[12px]">TRAC</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="w-full px-15 ">
-        <div className="border-b-1 border-[rgb(215,211,211)] px-10"></div>
+      <div className="hidden lg:block w-full lg:px-15 px-10 ">
+        <div className="border-b-1 border-[rgb(215,211,211)]"></div>
       </div>
 
+      <div className="block lg:hidden w-full">
+        {AccordianTitle.map((items, index) => (
+          <div key={index} className="px-10 py-10">
+            <AccordianFooter />
+          </div>
+        ))}
+      </div>
       <div className="w-full px-14 grid grid-cols-3 py-10 text-center text-[14px] text-gray-400 gap-5 ">
         <div className=" flex gap-2 items-center ">
           {PaymentIconsDatas.map((data, index) => (
@@ -127,9 +141,21 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <Drawer anchor="bottom" open={country} onClose={handelCountry} sx={{ "& .MuiDrawer-paper": { width: "25%", height:"auto" ,margin: "0 auto", borderRadius: "10px 10px 0 0", padding:"15px" } }}>
+      <Drawer
+        anchor="bottom"
+        open={country}
+        onClose={handelCountry}
+        sx={{
+          "& .MuiDrawer-paper": {
+            width: "25%",
+            height: "auto",
+            margin: "0 auto",
+            borderRadius: "10px 10px 0 0",
+            padding: "15px",
+          },
+        }}>
         <div className="text-black bg-white w-full m-auto">
-        <CountrySearch handleCountry={handelCountry} />
+          <CountrySearch handleCountry={handelCountry} />
         </div>
       </Drawer>
     </div>
