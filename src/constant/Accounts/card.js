1f7b565629aa36e83001a6122ba2812da98427db
SKRSUMAN <sumankhairba2002@gmail.com>
2025-03-06 12:43:15 +0530

    Updated code

---
+++ src/constant/Accounts/card.js
@@ -0,0 +1,10 @@
+export const cardDatas = [
+  {
+    bg: "/Images/account/b724fb0e9d606e2f03861992770feb7660514f59.jpeg",
+    title: "DOWNLOAD THE APP",
+  },
+  {
+    bg: "/Images/account/a859e630620508d6da81a8a54d8cc3533a2577c7.jpeg",
+    title: "FAQ",
+  },
+];
