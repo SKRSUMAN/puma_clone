8b1e8a069a441c5d57102dbabbd272252dd2318a
SUMAN KUMAR <113022724+SKRSUMAN@users.noreply.github.com>
2025-08-06 19:37:24 +0530

    Update layout.js
    lknm

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Toaster>
-          <Navbar />
+          // <Navbar />
           {children}
           <Footer />
         </Toaster>
